b9402c437b30a0e2065c70766e153320d6dc5fd8
Artur <artur.bula2001@gmail.com>
2022-11-09 19:01:45 +0100

    header+nav

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const navMobile = document.querySelector(".nav-mobile");
+const navBtn = document.querySelector(".hamburger");
+const footer = document.querySelector(".footer");
+const footerYear = document.querySelector(".footer__year");
+
+const handleNAv = () => {
+	navBtn.classList.toggle("is-active");
+	navMobile.classList.toggle("nav-mobile--active");
+	document.body.classList.toggle("body--hold");
+	footer.classList.toggle("footer--inactive");
+};
+
+navBtn.addEventListener("click", handleNAv);
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+handleCurrentYear();

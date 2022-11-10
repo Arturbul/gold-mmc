9e56a0671d8ff6d0310eee9c86d73786fb230508
Artur <artur.bula2001@gmail.com>
2022-11-10 13:20:20 +0100

    nav bug fixes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 const navMobile = document.querySelector(".nav-mobile");
 const navBtn = document.querySelector(".hamburger");
-const navLinks = document.querySelectorAll(".nav__link");
 const footer = document.querySelector(".footer");
 const footerYear = document.querySelector(".footer__year");
+const topBtn = document.querySelector(".top-btn");
 
+const navLinks = navMobile.querySelectorAll(".nav__link");
+
+const backToTop = () => {
+	// document.body.scrollTop = 0; // For Safari
+	// document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	window.location = "#header";
+};
 const handleNAv = () => {
 	navBtn.classList.toggle("is-active");
 	navMobile.classList.toggle("nav-mobile--active");
@@ -15,16 +22,32 @@ const handleNAv = () => {
 		}
 	});
 	if (navBtn.classList.contains("is-active")) {
-		// document.body.scrollTop = 0; // For Safari
-		// document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-		window.location = "#header";
+		backToTop();
 	}
 };
-
-navBtn.addEventListener("click", handleNAv);
-
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
 	footerYear.innerText = year;
 };
+
+const handleTopObserver = () => {
+	if (window.scrollY >= 100) {
+		topBtn.classList.add("top-btn--active");
+	} else {
+		topBtn.classList.remove("top-btn--active");
+	}
+};
+const handleNavObserver = () => {
+	if (
+		navMobile.classList.contains("nav-mobile--active") &&
+		window.innerWidth >= 992
+	) {
+		handleNAv();
+	}
+};
+
 handleCurrentYear();
+navBtn.addEventListener("click", handleNAv);
+topBtn.addEventListener("click", backToTop);
+window.addEventListener("scroll", handleTopObserver);
+window.addEventListener("resize", handleNavObserver);

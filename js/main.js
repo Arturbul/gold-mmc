c56146908d93a492218aaf172f0603e0da639e7b
Artur <artur.bula2001@gmail.com>
2022-11-10 11:19:13 +0100

    contact sec + nav bug

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 const navMobile = document.querySelector(".nav-mobile");
 const navBtn = document.querySelector(".hamburger");
+const navLinks = document.querySelectorAll(".nav__link");
 const footer = document.querySelector(".footer");
 const footerYear = document.querySelector(".footer__year");
 
 const handleNAv = () => {
 	navBtn.classList.toggle("is-active");
 	navMobile.classList.toggle("nav-mobile--active");
-	//po wybraniu freezuje
-	// document.body.classList.toggle("body--hold");
-	// footer.classList.toggle("footer--inactive");
+	document.body.classList.toggle("body--hold");
+	footer.classList.toggle("footer--inactive");
+	navLinks.forEach((link) => {
+		if (navMobile.classList.contains("nav-mobile--active")) {
+			link.addEventListener("click", handleNAv);
+		}
+	});
 };
 
 navBtn.addEventListener("click", handleNAv);
